09fece00c30349643ec687d07b6a20f3b5563821
Maxim Kuvardin <maxim@kuvard.in>
2022-08-25 02:01:21 +0600

    Enum PollType used in KeyboardButtonPollType

--- src/Types/KeyboardButtonPollType.php
+++ src/Types/KeyboardButtonPollType.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kuvardin\TelegramBotsApi\Types;
 
+use Kuvardin\TelegramBotsApi\Enums\PollType;
 use Kuvardin\TelegramBotsApi\Type;
 
 /**
@@ -16,23 +17,25 @@ use Kuvardin\TelegramBotsApi\Type;
 class KeyboardButtonPollType extends Type
 {
     /**
-     * @var string|null $type If <em>quiz</em> is passed, the user will be allowed to create only polls in the quiz
+     * @var PollType|null $type If <em>quiz</em> is passed, the user will be allowed to create only polls in the quiz
      *     mode. If <em>regular</em> is passed, only regular polls will be allowed. Otherwise, the user will be allowed
      *     to create a poll of any type.
      */
-    public ?string $type = null;
+    public ?PollType $type = null;
 
     public static function makeByArray(array $data): self
     {
         $result = new self;
-        $result->type = $data['type'] ?? null;
+        $result->type = isset($data['type']) ?
+            PollType::from($data['type'])
+            : null;
         return $result;
     }
 
     public function getRequestData(): array
     {
         return [
-            'type' => $this->type,
+            'type' => $this->type?->value,
         ];
     }
 }
